78d05f938d87665576c740e0aa33e03be1c0b3b5
tee29909 <59160571@go.buu.ac.th>
2020-03-23 09:35:55 +0700

    first commit

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -27,14 +27,19 @@
       <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
       <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
     </ul>
+    <MyName name =" ss"/>
   </div>
 </template>
 
 <script>
+import MyName from './MyName.vue'
 export default {
   name: 'HelloWorld',
   props: {
     msg: String
+  },
+   components: {
+    MyName
   }
 }
 </script>
---
+++ src/components/MyName.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <h1>{{name}}</h1>
+    
+  </div>
+</template>
+
+<script>
+
+
+export default {
+  name: 'MyName',
+  props: {
+      name: String
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
